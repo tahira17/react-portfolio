e342f2cdc5fcaec6354a44dd7cd57c49cd75d8f2
Tahira Nawaz <tahiranawaz@teresol.com>
2023-08-22 16:08:26 +0500

    email

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Skills from './components/Skills'
-import Work from './components/Work'
+// import Home from "./components/Home";
+// import NavBar from "./components/NavBar";
+// import Skills from './components/Skills'
+// import Work from './components/Work'
 
 
 function App() {
   return (
     <div  >
-<NavBar/>
+{/* <NavBar/>
 <Home />
 
 <About />
 <Skills />
-<Work />
+<Work /> */}
 <Contact />
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,107 @@
-import React from 'react'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import Button from "@material-ui/core/Button";
 
 const Contact = () => {
-  return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/d01cd87f-7f44-48c5-9f2c-51d3170faa83" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-purple-500 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'></p>
-            </div>
-            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='PH # 0331-5017691' name='name' disabled="true" />
-            <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email : tahiranawaz12345@gmail.com' name='email' disabled="true" />
-            {/* <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Send Email'></textarea>
-            <button className='text-white border-2 hover:bg-purple-500 hover:border-purple-500 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button> */}
-        </form>
-    </div>
-  )
-}
+  const [open, setOpen] = React.useState(false);
 
-export default Contact
+  const handleClickToOpen = () => {
+    setOpen(true);
+  };
+
+  const handleToClose = () => {
+    setOpen(false);
+  };
+  const form = useRef();
+
+  const sendemail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_6cb2by9",
+        "template_9iogpo8",
+        form.current,
+        "2NCi3llfjMC6yDg8x"
+      )
+      .then(
+        (result) => {
+          console.log(result.text, result.status);
+          // clearState();
+          handleClickToOpen();
+          dialogboxmsg="Success"
+        },
+        (error) => {
+          console.log(error.text);
+          handleClickToOpen();
+          dialogboxmsg="Failed"
+        }
+      );
+  };
+  return (
+    <div>
+      <div
+        name="contact"
+        className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      >
+        <form
+          className="flex flex-col max-w-[600px] w-full"
+          // onSubmit={sendemail}
+          ref={form}
+        >
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-purple-500 text-gray-300">
+              Contact
+            </p>
+            <p className="text-gray-300 py-4"></p>
+          </div>
+          <input
+            className="bg-[#ccd6f6] p-2"
+            type="text"
+            placeholder="Enter yout name"
+            required
+            name="name"
+          />
+          <input
+            className="my-4 p-2 bg-[#ccd6f6]"
+            type="email"
+            placeholder="Enter your email"
+            required
+            name="email"
+          />
+          <textarea
+            className="bg-[#ccd6f6] p-2"
+            name="message"
+            rows="10"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button onClick={handleClickToOpen} className="text-white border-2 hover:bg-purple-500 hover:border-purple-500 px-4 py-3 my-8 mx-auto flex items-center">
+            Let's Collaborate
+          </button>
+        </form>
+      </div>
+
+      {/* Dialog Box */}
+      <Dialog open={open} onClose={handleToClose}>
+        <DialogTitle >{dialogboxHeader}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Email sent Successfully.</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button onClick={handleToClose} className="border-2 hover:bg-purple-500 hover:border-purple-500 px-4 py-3 my-8 mx-auto flex items-center">
+            OK
+          </button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Contact;
